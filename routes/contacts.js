64a22ba4483800bdcf3939b1fe58482418935494
SpaceSettler <j.devon.richardson@gmail.com>
2025-02-01 01:16:36 -0700

    Wk3

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,5 +4,8 @@ const myController = require('../controllers/contacts');
 
 routes.get('/', myController.all);
 routes.get('/:id', myController.one);
+routes.post('/post', myController.postThis);
+routes.put('/put:id', myController.putThis);
+routes.delete('/delete:id', myController.deleteThis);
 
 module.exports = routes;
